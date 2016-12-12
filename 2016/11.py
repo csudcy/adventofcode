49198540cae70787b99614abab81903ce32b13b0
Nicholas Lee <csudcy@gmail.com>
2016-12-12 11:55:30 +0000

    Ideas for day 11

---
+++ 2016/11.py
@@ -0,0 +1,21 @@
+"""
+Initial setup:
+
+     1  2  3  4  5
+F4
+F3
+F2    M     M
+F1 E G  GM G  GM GM
+
+Make a simulator to check correctness & give it moves?
+
+U = 1
+D = -1
+G = 0
+M = 1
+MOVES = [
+    (U, (2,M), (,)),
+    (D, (2,M), (3,M)),
+    (U, (1,G), (,)),
+]
+"""
